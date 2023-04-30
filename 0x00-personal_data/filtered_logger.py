d2676c333be53835d099c2cde1f3a489046907bb
David Bakare <bakaredavid007@gmail.com>
2023-05-01 00:26:15 +0100

    added type hints

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,11 +4,12 @@ filter_datum module
 '''
 
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, seperator):
+def filter_datum(fields: str, redaction: str, message: List[str], seperator: str) -> str:  # noqa
     ''' returns the log message obfuscated'''
     for f in fields:
-        p = rf'''(?<={f}=)\w+|(?<=date_of_birth=)\d{{2}}/\d{{2}}/\d{{4}}'''
+        p = r'(?<=date_of_birth=)\d{2}/\d{2}/\d{4}' if f == 'date_of_birth' else rf'(?<={f}=)\w+'  # noqa
         message = re.sub(p, redaction, message)
     return message.replace(';', seperator)
