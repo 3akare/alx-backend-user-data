0450ee0367cd5cbfaa832b86e11fdf62316edb8d
David Bakare <bakaredavid007@gmail.com>
2023-05-01 00:45:14 +0100

    test 3

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,9 +7,15 @@ import re
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, seperator: str) -> str:  # noqa
-    ''' returns the log message obfuscated'''
-    for f in fields:
-        p: str = r'(?<=date_of_birth=)\d{2}/\d{2}/\d{4}' if f == 'date_of_birth' else rf'(?<={f}=)\w+'  # noqa
-        message = re.sub(p, redaction, message)
-    return message.replace(';', seperator)
+# def filter_datum(fields: List[str], redaction: str, message: str, seperator: str) -> str:  # noqa
+#     ''' returns the log message obfuscated'''
+#     for f in fields:
+#         p: str = r'(?<=date_of_birth=)\d{2}/\d{2}/\d{4}' if f == 'date_of_birth' else rf'(?<={f}=)\w+'  # noqa
+#         message = re.sub(p, redaction, message)
+#     return message.replace(';', seperator)
+
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:  # noqa
+    """Filters a log line."""
+    pattern = '|'.join(fields)
+    pattern = fr'(?P<field>{pattern})=[^{separator}]*'
+    return re.sub(pattern, f'\g<field>={redaction}', message).replace(';', separator)  # noqa
