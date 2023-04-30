1164827de3a7de5f7d938728693e89a4a92c3965
David Bakare <bakaredavid007@gmail.com>
2023-05-01 00:14:36 +0100

    filter datum

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,2 +1,14 @@
 #!/usr/bin/env python3
-print('hello world')
+'''
+filter_datum module
+'''
+
+import re
+
+
+def filter_datum(fields, redaction, message, seperator):
+    ''' returns the log message obfuscated'''
+    for f in fields:
+        p = rf'''(?<={f}=)\w+|(?<=date_of_birth=)\d{{2}}/\d{{2}}/\d{{4}}'''
+        message = re.sub(p, redaction, message)
+    return message.replace(';', seperator)
