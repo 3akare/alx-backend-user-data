2249feb36408d38ed453f1549083accdd8b64c1a
David Bakare <bakaredavid007@gmail.com>
2023-05-01 00:28:22 +0100

    added type hints 2

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -13,9 +13,3 @@ def filter_datum(fields: str, redaction: str, message: List[str], seperator: str
         p: str = r'(?<=date_of_birth=)\d{2}/\d{2}/\d{4}' if f == 'date_of_birth' else rf'(?<={f}=)\w+'  # noqa
         message = re.sub(p, redaction, message)
     return message.replace(';', seperator)
-
-fields = ["password", "date_of_birth"]
-messages = ["name=egg;email=eggmin@eggsample.com;password=eggcellent;date_of_birth=12/12/1986;", "name=bob;email=bob@dylan.com;password=bobbycool;date_of_birth=03/04/1993;"]
-
-for message in messages:
-    print(filter_datum(fields, 'xxx', message, ';'))
