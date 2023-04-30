1cbde14127d9c0cf142ababd60b8a95153fecafb
David Bakare <bakaredavid007@gmail.com>
2023-05-01 00:57:17 +0100

    redacting formatter clas

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,18 +4,29 @@ filter_datum module
 '''
 
 import re
+import logging
 from typing import List
 
-
-# def filter_datum(fields: List[str], redaction: str, message: str, seperator: str) -> str:  # noqa
-#     ''' returns the log message obfuscated'''
-#     for f in fields:
-#         p: str = r'(?<=date_of_birth=)\d{2}/\d{2}/\d{4}' if f == 'date_of_birth' else rf'(?<={f}=)\w+'  # noqa
-#         message = re.sub(p, redaction, message)
-#     return message.replace(';', seperator)
 
 def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:  # noqa
     """Filters a log line."""
     pattern = '|'.join(fields)
     pattern = fr'(?P<field>{pattern})=[^{separator}]*'
     return re.sub(pattern, f'\g<field>={redaction}', message).replace(';', separator)  # noqa
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]) -> None:
+        self.fields = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Filters a log line."""
+        return filter_datum(self.fields, self.REDACTION, super(RedactingFormatter, self).format(record), self.SEPARATOR)  # noqa
