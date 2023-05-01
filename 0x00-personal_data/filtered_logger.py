1deb829f3d20bcae1478f48cf350a36b0303c280
David Bakare <bakaredavid007@gmail.com>
2023-05-01 13:23:38 +0100

    added get_logger() function

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,12 +7,23 @@ import re
 import logging
 from typing import List
 
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
+
 
 def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:  # noqa
     """Filters a log line."""
     pattern = '|'.join(fields)
     pattern = fr'(?P<field>{pattern})=[^{separator}]*'
     return re.sub(pattern, f'\g<field>={redaction}', message).replace(';', separator)  # noqa
+
+
+def get_logger() -> logging.Logger:
+    '''A logger function'''
+    logger = logging.getLogger('user_data')
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    logger.addHandler(logging.StreamHandler().setFormatter(RedactingFormatter(PII_FIELDS)))  # noqa
+    return logger
 
 
 class RedactingFormatter(logging.Formatter):
