22b22e48aa14d5562241497370ad920da686ec17
David Bakare <bakaredavid007@gmail.com>
2023-05-04 21:13:43 +0100

    added BasicAuth Class II

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,11 +3,13 @@
 Basic Authentication Class Module
 '''
 try:
+    # for flask
     from api.v1.auth.auth import Auth
 except Exception:
     pass
 
 try:
+    # for testing the class
     from auth import Auth
 except Exception:
     pass
