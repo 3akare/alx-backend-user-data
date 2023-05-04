17a7e6d8f18aae9c471b19ac04197dc8ca694847
David Bakare <bakaredavid007@gmail.com>
2023-05-04 15:55:09 +0100

    back to the basics

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -1,33 +1,26 @@
 #!/usr/bin/env python3
+""" Module of Index views
 """
-Route module for the API
-"""
-from os import getenv
+from flask import jsonify, abort
 from api.v1.views import app_views
-from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
-import os
-
-app = Flask(__name__)
-app.register_blueprint(app_views)
-CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
-@app.errorhandler(404)
-def not_found(error) -> str:
-    """ Not found handler
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status() -> str:
+    """ GET /api/v1/status
+    Return:
+      - the status of the API
     """
-    return jsonify({"error": "Not found"}), 404
+    return jsonify({"status": "OK"})
 
 
-@app.errorhandler(401)
-def not_authorized(error) -> str:
-    """ Not found handler
+@app_views.route('/stats/', strict_slashes=False)
+def stats() -> str:
+    """ GET /api/v1/stats
+    Return:
+      - the number of each objects
     """
-    return jsonify({"error": "unauthorized"}), 401
-
-
-if __name__ == "__main__":
-    host = getenv("API_HOST", "0.0.0.0")
-    port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port, debug=True)
+    from models.user import User
+    stats = {}
+    stats['users'] = User.count()
+    return jsonify(stats)
