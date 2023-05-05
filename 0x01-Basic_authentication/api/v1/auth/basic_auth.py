606f6f428621566e21d8fe7b761bb3e1a0965335
David Bakare <bakaredavid007@gmail.com>
2023-05-05 01:08:09 +0100

    probably solved the password ':'

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -52,7 +52,7 @@ class BasicAuth(Auth):
             return (None, None)
         if ':' not in decode_base64_authorization_header:
             return (None, None)
-        credentials = decode_base64_authorization_header.split(':')
+        credentials = decode_base64_authorization_header.split(':', maxsplit=1)
         return (credentials[0], credentials[1])
 
     def user_object_from_credentials(self, user_email: str, user_pwd: str) -> TypeVar('User'):  # noqa
