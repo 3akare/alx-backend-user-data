fc4545db4913926dcffbb9b662d9382a53d61031
David Bakare <bakaredavid007@gmail.com>
2023-05-05 21:08:22 +0100

    current_user method override

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,8 +3,9 @@
 Session Authentication Module
 '''
 
-from api.v1.auth.auth import Auth
-# from auth import Auth
+# from api.v1.auth.auth import Auth
+from models.users import User
+from auth import Auth
 from uuid import uuid4
 
 
@@ -32,3 +33,10 @@ class SessionAuth(Auth):
         if session_id is None or type(session_id) != str:
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        '''
+        Get current user from user_id_session and session.cookie
+        '''
+        user_id = self.user_id_for_session_id(self.session_cookie(request))
+        return User.get(user_id)
