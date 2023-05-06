6639b7fb3f1b2d75c26c092e606a91b42ddc0850
David Bakare <bakaredavid007@gmail.com>
2023-05-06 15:43:17 +0100

    ...

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -39,5 +39,8 @@ class SessionAuth(Auth):
         '''
         Get current user from user_id_session and session cookie
         '''
-        user_id = self.user_id_for_session_id(self.session_cookie(request))
-        return User.get(user_id)
+        if request:
+            session_cookie = self.session_cookie(request)
+            if session_cookie:
+                user_id = self.user_id_for_session_id(session_cookie)
+                return User.get(user_id)
