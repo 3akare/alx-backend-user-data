24e183fe6cc121c02ac59af77ca236186e4a44bb
David Bakare <bakaredavid007@gmail.com>
2023-05-12 18:17:42 +0100

    reset password

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -48,6 +48,19 @@ def login() -> str:
     return response
 
 
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout() -> str:
+    '''
+    Destroys a user's session and redirect the user to the index page
+    '''
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        abort(403)
+    AUTH.destroy_session(user.id)
+    return redirect("/")
+
+
 @app.route('/profile', methods=['GET'], strict_slashes=False)
 def profile() -> str:
     '''
@@ -60,17 +73,19 @@ def profile() -> str:
     return jsonify({"email": user.email})
 
 
-@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
-def logout() -> str:
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+def get_token() -> str:
     '''
-    Destroys a user's session and redirect the user to the index page
+    get token
     '''
-    session_id = request.cookies.get("session_id")
-    user = AUTH.get_user_from_session_id(session_id)
-    if user is None:
+    email = request.form.get('email')
+    try:
+        token = AUTH.get_reset_password_token(email)
+        if token is None:
+            abort(403)
+        return jsonify({"email": email, "reset_token": token})
+    except ValueError:
         abort(403)
-    AUTH.destroy_session(user.id)
-    return redirect("/")
 
 
 if __name__ == '__main__':
